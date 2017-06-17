463a1ba2b511f941d8a99b9945a3cff3d30fc8c5
Peijun Zhu <zpj.ustc@gmail.com>
2017-06-16 21:06:29 -0400

    MPO

--- MPO.py
+++ MPO.py
@@ -1,84 +1,137 @@
-from numpy import zeros, array, einsum, tensordot, matmul
-import math
-from scipy.sparse.linalg import eigsh
-from numpy.linalg import eigh, norm, svd
-from scipy.sparse.linalg import svds
-
-G = 0.1
-J = 1.0
-D = 10
-s0 = array([[1, 0], [0, 1]], dtype='int8')
-sx = array([[0, 1], [1, 0]], dtype='int8')
-sz = array([[1, 0], [0, -1]], dtype='int8')
-
-M = zeros([3, 3, 2, 2], dtype='double')
-M[0, 0] = s0
-M[2, 2] = s0
-M[2, 0] = -G * sx
-M[2, 1] = sz
-M[1, 0] = -J * sz
-
-S = 2
-
-L = zeros([1, 1, 3])
-R = zeros([1, 1, 3])
-
-L[:, :, -1] = 1
-R[:, :, 0] = 1
-
-#
+import numpy as np
+from ast import literal_eval
+op2 = {'s0': np.array([[1, 0], [0, 1]], dtype='double'),
+       'sx': np.array([[0, 1], [1, 0]], dtype='double'),
+       'sy': np.array([[0, -1j], [1j, 0]], dtype='complex'),
+       'sz': np.array([[1, 0], [0, -1]], dtype='double')
+       }
 
 
-def isqrt(n):
-    return round(math.sqrt(n))
+class MPO:
+    '''x是行，y是列, op是算符。默认(0,0)和(n,n)有两个恒等算符'''
+    width = 4
+    lazy = True
 
+    def __init__(self, name='s0', op=None, dim=2):
+        self.x = [1]
+        self.y = [0]
+        #assert(op.shape==(dim, dim))
+        self.op = [op]
+        self.name = [name]
+        self.dof = 0
+        self.dim = dim
+        self.N = 1
 
-def halve(psi, sh):
-    '''SVD psi'''
-    lsh = sh[:2]
-    rsh = sh[2:]
-    lsize = lsh[0] * lsh[1]
-    rsize = rsh[0] * rsh[1]
-    A = psi.reshape([lsize, rsize])
-    U, S, V = svd(A, full_matrices=False)
-    if S.size > D:
-        d = D
-        U = U[:, :D]
-        V = V[:D]
-    else:
-        d = S.size
-    U = U.reshape([*lsh, d])
-    V = V.reshape([d, *rsh])
-    print(U.size)
-    return U, S, V
+    def eval_op(self):
+        for i in range(self.N):
+            if self.op[i] is None:
+                self.op[i] = eval(self.name[i], op2).copy()
 
+    def __str__(self):
+        M = self.tomatrix_name()
+        return '\n'.join(
+            [''.join(['{:^{}}'.format(j, MPO.width) for j in i]) for i in M]
+        )
 
-def H(L, M, R):
-    return einsum('Aa i, ij Bb, jk Cc,Dd k->ABCDabcd', L, M, M, R)
+    def tomatrix_name(self):
+        M = np.zeros([self.dof + 2, self.dof + 2], dtype='U10')
+        M[0, 0] = '1'
+        M[-1, -1] = '1'
+        for i in range(self.dof):
+            M[i + 1, i + 1] = '↰'
+        for x, y, op in zip(self.x, self.y, self.name):
+            if M[x, y] and not (op.startswith('+') or op.startswith('-')):
+                M[x, y] += '+' + op
+            else:
+                M[x, y] += op
+        return M
 
+    def tomatrix(self, dtype='complex', names=op2):
+        M = np.zeros([self.dof + 2, self.dof + 2,
+                      self.dim, self.dim], dtype=dtype)
+        M[0, 0] = np.eye(self.dim)
+        M[-1, -1] = np.eye(self.dim)
+        self.eval_op()
+        for x, y, op in zip(self.x, self.y, self.op):
+            M[x, y] += op
+        return M
 
-def halfshape(A):
-    return A.shape[:4]
+    def __repr__(self):
+        return self.__str__()
 
+    def __iadd__(self, rhs):
+        L = self.dof + rhs.dof
+        self.x = [i if i <= self.dof else L + 1 for i in self.x]
+        x2 = [i + self.dof for i in rhs.x]
+        y2 = [i + self.dof if i else 0 for i in rhs.y]
+        self.x += x2
+        self.y += y2
+        self.dof += rhs.dof
+        self.op += rhs.op
+        self.N += rhs.N
+        self.name += rhs.name
+        return self
 
-def matrixify(A):
-    n = isqrt(A.size)
-    return A.reshape([n, n])
+    def __add__(self, rhs):
+        ret = self.copy()
+        ret += rhs
+        return ret
 
+    def __sub__(self, rhs):
+        ret = self.copy()
+        ret += (-1) * rhs
+        return ret
 
-def next_LR(L, R, M):
-    '''L的三个指标分别是上下中
-    U分别是左下右，V也是左下右
-    M分别是左右上下
-    L上对U左，L下对U*左'''
-    A = H(L, M, R)
-    val, vec = eigsh(matrixify(A), k=1, which='SA')
-    U, S, V = halve(vec, halfshape(A))
-    L = einsum('ijk, ilA, jmB, kClm', L, U, U.conj(), M)
-    R = einsum('ijk, Ali, Bmj, Cklm', R, V, V.conj(), M)
-    return L, R, val
+    def __neg__(self):
+        return (-1) * self
 
+    def __imul__(self, rhs):
+        assert(self.dim == rhs.dim)
+        self.x = [i + rhs.dof + 1 for i in self.x]
+        self.y = [i + rhs.dof + 1 for i in self.y]
+        self.x += rhs.x
+        self.y += rhs.y
+        self.dof += rhs.dof + 1
+        self.op += rhs.op
+        self.N += rhs.N
+        self.name += rhs.name
+        return self
 
-for i in range(20):
-    L, R, val = next_LR(L, R, M)
-    print('Energy is', val)
+    def __rmul__(self, lhs):
+        ret = self.copy()
+        ret.eval_op()
+        for i in range(self.N):
+            if ret.y[i] == 0:
+                ret.op[i] *= lhs
+        return ret
+
+    def __div__(self, rhs):
+        ret = self.copy()
+        ret.eval_op()
+        for i in range(self.N):
+            if ret.y[i] == 0:
+                ret.op[i] /= lhs
+        return ret
+
+    def __mul__(self, rhs):
+        ret = self.copy()
+        ret *= rhs
+        return ret
+
+    def __pow__(self, n):
+        ret = self.copy()
+        for i in range(n - 1):
+            ret *= self
+        return ret
+
+    def copy(self):
+        new = MPO()
+        new.x = self.x.copy()
+        new.y = self.y.copy()
+        new.op = self.op.copy()
+        new.name = self.name.copy()
+        new.dof = self.dof
+        new.N = self.N
+        new.dim = self.dim
+        return new
+# 数乘，整数幂
--- MPS.py
+++ MPS.py
@@ -4,6 +4,7 @@ import numpy.linalg as la
 from numpy.linalg import eigh, norm, svd
 from functools import reduce
 
+
 class MPS:
     '''How to construct?
     + Given chi and construct basic shape
@@ -15,6 +16,7 @@ class MPS:
     + chi: L+1
     + v: L+2
     '''
+
     def __init__(self, chi, dim=2, orth=True):
         '''chi is an array of truncation length with length n+1,
         so we have chi[0]...chi[n] and has shape (chi[i], chi[i+1])
@@ -24,130 +26,147 @@ class MPS:
         some boundary open, for example chi[n] > 1, then they is a series of MPS,
         which may be used to construct some other MPS on contraction.
         '''
-        chi=list(chi)
-        self.L=len(chi)-1
+        chi = list(chi)
+        self.L = len(chi) - 1
         for i in range(self.L):
-            chi[i+1]=min(2*chi[i], chi[i+1])
-            chi[self.L-(i+1)]=min(2*chi[self.L-i], chi[self.L-(i+1)])
-        self.chi=chi
-        self.M=[None]*(self.L+2)
-        self.M[0]=np.ones([1,1,1], dtype='complex')
-        self.M[-1]=np.ones([1,1,1], dtype='complex')
-        self.M[1:-1]=[zeros([chi[i], dim, chi[i+1]], dtype='complex') for i in range(self.L)]
-        self.dim=dim
-        self.status='raw'
-        self.lamb=[None]*(self.L+1)
-        self.lamb[0]=np.ones([1])
-        self.lamb[-1]=np.ones([1])
+            chi[i + 1] = min(2 * chi[i], chi[i + 1])
+            chi[self.L - (i + 1)] = min(2 * chi[self.L - i],
+                                        chi[self.L - (i + 1)])
+        self.chi = chi
+        self.M = [None] * (self.L + 2)
+        self.M[0] = np.ones([1, 1, 1], dtype='complex')
+        self.M[-1] = np.ones([1, 1, 1], dtype='complex')
+        self.M[1:-1] = [zeros([chi[i], dim, chi[i + 1]],
+                              dtype='complex') for i in range(self.L)]
+        self.dim = dim
+        self.status = 'raw'
+        self.lamb = [None] * (self.L + 1)
+        self.lamb[0] = np.ones([1])
+        self.lamb[-1] = np.ones([1])
+
     def graph(self):
         ''' 1--v--2--v--1
                |     |
                2     2
         '''
-        s='--▼--'.join(str(i) for i in self.chi)
-        w=''.join(['|' if i=='▼' else ' ' for i in s])
-        n=w.replace('  |', '{:3d}'.format(self.dim))
-        return '\n'.join((s,w,n))
+        s = '--▼--'.join(str(i) for i in self.chi)
+        w = ''.join(['|' if i == '▼' else ' ' for i in s])
+        n = w.replace('  |', '{:3d}'.format(self.dim))
+        return '\n'.join((s, w, n))
+
     def shape(self, i):
-        return self.chi[i], self.dim, self.chi[i+1]
+        return self.chi[i], self.dim, self.chi[i + 1]
+
     def ortho_left_site(self, i, cut=True, err=1e-16):
-        before = (-1, self.chi[i+1])
+        before = (-1, self.chi[i + 1])
         after = (self.chi[i], self.dim, -1)
-        m=self.M[i+1].reshape(before)
-        u, s, v=svd(m, full_matrices=False)
+        m = self.M[i + 1].reshape(before)
+        u, s, v = svd(m, full_matrices=False)
         if cut:
-            #print(s)
-            ind=sum(np.abs(s)>err)
-            u=u[:, :ind]
-            v=v[:ind]
-            s=s[:ind]
-            self.chi[i+1]=ind
-        self.M[i+1]=u.reshape(after)
+            # print(s)
+            ind = sum(np.abs(s) > err)
+            u = u[:, :ind]
+            v = v[:ind]
+            s = s[:ind]
+            self.chi[i + 1] = ind
+        self.M[i + 1] = u.reshape(after)
         return s, v
+
     def ortho_right_site(self, i, cut=True, err=1e-16):
         before = (self.chi[i], -1)
-        after = (-1, self.dim, self.chi[i+1])
-        m=self.M[i+1].reshape(before)
-        u, s, v=svd(m, full_matrices=False)
+        after = (-1, self.dim, self.chi[i + 1])
+        m = self.M[i + 1].reshape(before)
+        u, s, v = svd(m, full_matrices=False)
         if cut:
-            ind=sum(np.abs(s)>err)
-            u=u[:, :ind]
-            v=v[:ind]
-            s=s[:ind]
-            self.chi[i]=ind
-        self.M[i+1]=v.reshape(after)
+            ind = sum(np.abs(s) > err)
+            u = u[:, :ind]
+            v = v[:ind]
+            s = s[:ind]
+            self.chi[i] = ind
+        self.M[i + 1] = v.reshape(after)
         return u, s
+
     def ortho_left(self, cen=None):
         '''Orthonormalization from the left the the end'''
         if cen is None:
-            cen=self.L
-        assert(self.chi[0]==1)
+            cen = self.L
+        assert(self.chi[0] == 1)
         for i in range(cen):
             #print('Before:', np.einsum('lcr, rst->lcst', self.M[i+1], self.M[i+2]))
             s, v = self.ortho_left_site(i)
-            self.lamb[i+1]=s
-            v*=s[:, np.newaxis]
-            self.M[i+2]=np.einsum('al, lcr->acr', v, self.M[i+2])
+            self.lamb[i + 1] = s
+            v *= s[:, np.newaxis]
+            self.M[i + 2] = np.einsum('al, lcr->acr', v, self.M[i + 2])
             #print('After:', np.einsum('lcr, rst->lcst', self.M[i+1], self.M[i+2]))
-        if cen==self.L and self.lamb[-1][0]<0:
-            self.lamb[-1][0]*=-1
-            self.M[self.L]*=-1
-            self.M[self.L+1]*=-1
+        if cen == self.L and self.lamb[-1][0] < 0:
+            self.lamb[-1][0] *= -1
+            self.M[self.L] *= -1
+            self.M[self.L + 1] *= -1
         assert(self.verify_shape())
+
     def ortho_right(self, cen=0):
-        assert(self.chi[-1]==1)
-        for i in range(self.L-1, cen-1, -1):
-            u, s=self.ortho_right_site(i)
-            self.lamb[i]=s
-            u*=s[np.newaxis, :]
-            self.M[i]=np.einsum('lcr, ra->lca', self.M[i], u)
-        if cen==0 and self.M[0]<0:
-            self.M[0]*=-1
-            self.M[1]*=-1
+        assert(self.chi[-1] == 1)
+        for i in range(self.L - 1, cen - 1, -1):
+            u, s = self.ortho_right_site(i)
+            self.lamb[i] = s
+            u *= s[np.newaxis, :]
+            self.M[i] = np.einsum('lcr, ra->lca', self.M[i], u)
+        if cen == 0 and self.M[0] < 0:
+            self.M[0] *= -1
+            self.M[1] *= -1
         assert(self.verify_shape())
+
     def vidalize(self):
         '''Gamma*Lambda=B'''
         self.ortho_left()
         self.ortho_right()
-        self.lamb[-1][0]=1
+        self.lamb[-1][0] = 1
         for i in range(0, self.L):
-            self.M[i+1]/=self.lamb[i+1][np.newaxis, np.newaxis, :]
-        assert(np.angle(s.M[0])==0 and np.angle(s.M[-1])==0)
+            self.M[i + 1] /= self.lamb[i + 1][np.newaxis, np.newaxis, :]
+        assert(np.angle(s.M[0]) == 0 and np.angle(s.M[-1]) == 0)
+
     def A(self, i):
-        return self.M[i+1]*self.lamb[i][:, np.newaxis, np.newaxis]
+        return self.M[i + 1] * self.lamb[i][:, np.newaxis, np.newaxis]
+
     def B(self, i):
-        return self.M[i+1]*self.lamb[i+1][np.newaxis, np.newaxis, :]
+        return self.M[i + 1] * self.lamb[i + 1][np.newaxis, np.newaxis, :]
+
     def verify_shape(self):
         for i in range(self.L):
-            expect=self.shape(i)
-            got=self.M[i+1].shape
+            expect = self.shape(i)
+            got = self.M[i + 1].shape
             if expect != got:
                 print('Shape error at {}, expected {}, got {}'.format(i, expect, got))
                 return False
         return True
+
     def __repr__(self):
         return self.graph()
 
     def dot(self, rhs):
         '''Inner product between two wavefunction'''
         pass
+
     def wave(self):
         '''Get the wave function'''
         pass
+
     def __getitem__(self, ind):
         print(ind)
-        return reduce(matmul, (self.M[i][ind[i]] for i in range(self.dim)))[0,0]
+        return reduce(matmul, (self.M[i][ind[i]] for i in range(self.dim)))[0, 0]
+
     def __add__(self, m):
         l, d, r = m.shape
-        if d==self.dim and l == self.chi[-1]:
+        if d == self.dim and l == self.chi[-1]:
             self.M.append(m)
             self.chi.append(l2)
+
+
 if __name__ == "__main__":
-    s=MPS((1,100,1))
-    s.M[1][0, :, :]=array([[0,2+1j],[1,0]])
-    s.M[2][:, :, 0]=array([[5-0.3j,2],[0.3,4.3]])
+    s = MPS((1, 100, 1))
+    s.M[1][0, :, :] = array([[0, 2 + 1j], [1, 0]])
+    s.M[2][:, :, 0] = array([[5 - 0.3j, 2], [0.3, 4.3]])
     np.set_printoptions(precision=5)
     s.vidalize()
     print(s.M)
     print(s.lamb)
-
