299527b82979a8736bcc0b76c279415c0e6bbf34
Peijun Zhu <zpj.ustc@gmail.com>
2017-06-17 21:49:21 -0400

    Unleashed closed boundary constraint

--- MPS.py
+++ MPS.py
@@ -10,13 +10,15 @@ from functools import reduce
 
 class State:
     '''Length:
-    + o: L+1
+    + o: L-1
+    + v: L
     + chi: L+1
-    + v: L+2
     '''
 
-    def __init__(self, chi=None, M=None, dim=2):
-        '''chi is an array of truncation length with length n+1,
+    def __init__(self, arg=(1, 1), dim=2):
+        '''
+        arg may be chi or M, only one of them is needed.
+        chi is an array of truncation length with length n+1,
         so we have chi[0]...chi[n] and has shape (chi[i], chi[i+1])
         for M[i] where i is in range(0, n).
 
@@ -24,47 +26,43 @@ class State:
         some boundary open, for example chi[n] > 1, then they is a series of MPS,
         which may be used to construct some other MPS on contraction.
         '''
-        chi = list(chi)
-        self.L = len(chi) - 1
-        self.chi = chi
-        self.M = [None] * (self.L + 2)
-        self.M[0] = np.ones([1, 1, 1], 'complex')
-        self.M[-1] = np.ones([1, 1, 1], 'complex')
-        self.M[1:-1] = [np.zeros([l, dim, r], 'complex')
-                        for l, r in zip(chi[:-1], chi[1:])]
         self.dim = dim
-        self.status = 'raw'
-        self.lamb = [None] * (self.L + 1)
-        self.lamb[0] = np.ones([1])
-        self.lamb[-1] = np.ones([1])
+        if isinstance(arg[0], np.ndarray):
+            self.from_M(arg)
+        else:
+            self.from_chi(arg)
+        self.lamb = [None] * (self.L - 1)
         self.canonical = False
 
-    def from_chi(self):
-        pass
+    def from_chi(self, chi):
+        self.chi = list(chi)
+        self.M = [np.zeros([l, self.dim, r], 'complex')
+                  for l, r in zip(chi[:-1], chi[1:])]
+        self.L = len(chi) - 1
 
-    def from_M(self):
-        pass
+    def from_M(self, M):
+        L = [m.shape[0] for m in M]
+        R = [m.shape[0] for m in M]
+        assert L[1:] == R[:-1], "Incompatible matrices!"
+        L.append(R[-1])
+        self.chi = L
+        self.M = [np.array(i, 'complex') for i in M]
+        self.L = len(M)
 
     @staticmethod
-    def naive(L):
+    def naive(*L):
         '''L is N*s matrix, chi are ones'''
-        n = len(L)
-        chi = [1] * (n + 1)
-        s = State(chi)
-        L = np.array(L)
-        for i in range(n):
-            s.M[i + 1] = L[i].reshape((1, 2, 1))
-        return s
+        L = np.array(L).reshape([-1, 1, 2, 1])
+        return State(L)
 
     def graph(self):
         '''
-            1--▼--2--▼--1
-               |     |
-               2     2
-            1--v--2--v--1
+        1 -- v -- 2 -- v -- 1
+             |         |
+             2         2
         '''
-        s = '--▼--'.join(str(i) for i in self.chi)
-        w = ''.join(['|' if i == '▼' else ' ' for i in s])
+        s = '-- v --'.join('{:^3}'.format(i) for i in self.chi)
+        w = ''.join(['|' if i == 'v' else ' ' for i in s])
         n = w.replace('  |  ', '{:^5}'.format(self.dim))
         return '\n'.join((s, w, n))
 
@@ -81,7 +79,7 @@ class State:
         '''Left orthogonalize site i and optimize left chi[i+1]'''
         before = (-1, self.chi[i + 1])
         after = (self.chi[i], self.dim, -1)
-        m = self.M[i + 1].reshape(before)
+        m = self.M[i].reshape(before)
         u, s, v = la.svd(m, full_matrices=False)
         if cut:
             ind = sum(np.abs(s) > err)
@@ -89,14 +87,14 @@ class State:
             v = v[:ind]
             s = s[:ind]
             self.chi[i + 1] = ind
-        self.M[i + 1] = u.reshape(after)
+        self.M[i] = u.reshape(after)
         return s, v
 
     def ortho_right_site(self, i, cut=True, err=1e-16):
         '''Right orthogonalize site i and optimize left chi[i]'''
         before = (self.chi[i], -1)
         after = (-1, self.dim, self.chi[i + 1])
-        m = self.M[i + 1].reshape(before)
+        m = self.M[i].reshape(before)
         u, s, v = la.svd(m, full_matrices=False)
         if cut:
             ind = sum(np.abs(s) > err)
@@ -104,82 +102,72 @@ class State:
             v = v[:ind]
             s = s[:ind]
             self.chi[i] = ind
-        self.M[i + 1] = v.reshape(after)
+        self.M[i] = v.reshape(after)
+        print(self.M[i].dtype, v.dtype)
+        print('After M = {}, v = {}'.format(self.M[i], v.reshape(after)))
         return u, s
 
     def ortho_left(self, cen=None):
         '''Go from the left end to left orthogonalize till center'''
         if cen is None:
-            cen = self.L
-        assert self.chi[0] == 1, "Left end should be closed!"
+            cen = self.L - 1
         for i in range(cen):
-            #print('Before:', np.einsum('lcr, rst->lcst', self.M[i+1], self.M[i+2]))
             s, v = self.ortho_left_site(i)
-            self.lamb[i + 1] = s
+            s /= la.norm(s)
+            self.lamb[i] = s
             v *= s[:, np.newaxis]
-            self.M[i + 2] = np.einsum('al, lcr->acr', v, self.M[i + 2])
-            #print('After:', np.einsum('lcr, rst->lcst', self.M[i+1], self.M[i+2]))
-        if cen == self.L and self.lamb[-1][0] < 0:
-            self.lamb[-1][0] *= -1
-            self.M[self.L] *= -1
-            self.M[self.L + 1] *= -1
+            self.M[i + 1] = np.einsum('al, lcr->acr', v, self.M[i + 1])
         self.verify_shape()
 
     def ortho_right(self, cen=0):
         '''Go from the right end to right orthogonalize till center'''
-        assert self.chi[-1] == 1, "Right end should be closed!"
-        for i in range(self.L - 1, cen - 1, -1):
+        for i in range(self.L - 1, cen, -1):
             u, s = self.ortho_right_site(i)
-            self.lamb[i] = s
+            print('-' * 30)
+            s /= la.norm(s)
+            self.lamb[i - 1] = s
             u *= s[np.newaxis, :]
-            self.M[i] = np.einsum('lcr, ra->lca', self.M[i], u)
-        if cen == 0 and self.M[0] < 0:
-            self.M[0] *= -1
-            self.M[1] *= -1
+            self.M[i - 1] = np.einsum('lcr, ra->lca', self.M[i - 1], u)  # TODO
         self.verify_shape()
 
     def canon(self):
         '''Decompose MPS into ΓΛΓΛΓΛΓΛΓ'''
         self.ortho_left()
         self.ortho_right()
-        self.lamb[-1][0] = 1
-        for i in range(0, self.L):
-            self.M[i + 1] /= self.lamb[i + 1][np.newaxis, np.newaxis, :]
-        assert (np.angle(self.M[0]) == 0 and np.angle(
-            self.M[-1]) == 0), "Phase != 0 at ends!"
+        for i in range(0, self.L - 1):
+            print('Shape of M', self.M[i].shape)
+            self.M[i] /= self.lamb[i][np.newaxis, np.newaxis, :]
         self.canonical = True
-
-    def S(self, i):
-        '''Safe Matrix visit'''
-        if self.canonical:
-            return self.A(i)
-        else:
-            return self.M[i + 1]
 
     def A(self, i):
         '''Left Matrix after Orthonormalization'''
-        return self.M[i + 1] * self.lamb[i][:, np.newaxis, np.newaxis]
+        if i == 0:
+            return self.M[0]
+        else:
+            return self.M[i] * self.lamb[i - 1][:, np.newaxis, np.newaxis]
 
     def B(self, i):
         '''Left Matrix after Orthonormalization'''
-        return self.M[i + 1] * self.lamb[i + 1][np.newaxis, np.newaxis, :]
+        if i == self.L - 1:
+            return self.M[-1]
+        else:
+            return self.M[i] * self.lamb[i][np.newaxis, np.newaxis, :]
 
     def verify_shape(self):
         '''Verify that chi is compatible with M'''
         for i in range(self.L):
             expect = self.shape(i)
-            got = self.M[i + 1].shape
+            got = self.M[i].shape
             assert expect == got, 'Shape error at {}, expected {}, got {}'.format(
                 i, expect, got)
 
     def dot(self, rhs):
         '''Inner product between two wavefunctions'''
         assert self.dim == rhs.dim, "Shape conflict between states"
-        E = np.einsum('lcr, icj->rj', self.S(0).conj(), rhs.S(0))
+        E = np.einsum('lcr, lcj->rj', self.S(0).conj(), rhs.S(0))
         for i in range(1, self.L):
-            print(E)
             E = np.einsum('kl, kno, lnr->or', E, self.S(i).conj(), rhs.S(i))
-        return E[0, 0]
+        return np.trace(E)
 
     def wave(self):
         '''Get the wave function'''
@@ -210,27 +198,23 @@ class State:
             x = i + 1
             E = np.einsum('kl, kio, ij, ljr->or',
                           E, self.A(i).conj(), op, self.A(i))
-        return np.einsum('ii, i', E, self.lamb[i + 1]**2).real
+        return np.einsum('ii, i', E, self.lamb[i]**2).real
 
-    def update(sites, U):
+    def update(U, *sites):
         '''Apply U at sites, there are 2*len(sites) axes in U'''
         assert(len(sites) * 2 == len(U.shape))
-        assert()
+        if sites:
+            pass
 
 
 if __name__ == "__main__":
     from pauli import sigma
-    s = State((1, 2, 1))
-    s.M[1][0, :, :] = np.array([[0, 2 + 1], [1, 0]])
-    s.M[2][:, :, 0] = np.array([[5 - 1, 2 - 3j], [0.3, 4]])
-    np.set_printoptions(precision=5)
-    s.canon()
-    print(s.lamb[1])
-    #print(s.measure((0, sigma[3])))
-    # print(s.lamb[1]**2)
-    # print(s.dot(s))
-    # print(s[[0,1],[0,1]])
-    # print(s[[0,1],[1,0]])
-    # print(s[[1,0],[0,1]])
-    # print(s[[1,0],[1,0]])
-    print(0.317588170991**2 + 0.414245440423**2 + 0.666302284103**2)
+    #s = State((1, 2, 1))
+    #s.M[0][0, :, :] = np.array([[5, 2 + 1], [1, 6j]])
+    #s.M[1][:, :, 0] = np.array([[5 - 1j, 2 - 3j], [0.3, 4]])
+    # np.set_printoptions(precision=5)
+    # s.canon()
+    # s.dot(s)
+    r = State.naive([0, 1], [1, 0], [2, 3])
+    r.canon()
+    print(r.lamb)
