35cc9f7ae8531c3b8f1462dbf6a092bc2d3e5859
Peijun Zhu <zpj.ustc@gmail.com>
2018-02-01 15:40:00 -0500

    add test

---
+++ DMRG/test_spin.py
@@ -0,0 +1,14 @@
+'''Test commutation relation'''
+from .spin import sigma
+import numpy as np
+
+def test_pauli():
+    np.testing.assert_allclose(sigma[0], np.eye(2))
+    s = sigma[1:]
+    for i in range(3):
+        for j in range(3):
+            anti = s[i]@s[j]+s[j]@s[i]
+            np.testing.assert_allclose(anti,2*((i == j)*np.eye(2)))
+    for i in range(3):
+        comm = s[i-1]@s[i]-s[i]@s[i-1]
+        np.testing.assert_allclose(2j*s[i-2], comm)
